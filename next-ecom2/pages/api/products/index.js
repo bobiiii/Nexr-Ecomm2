557d74c211ab63efa42c66e72b96592592d78a50
bobiiii <52751757+bobiiii@users.noreply.github.com>
2022-06-27 07:04:28 -0700

    added Added Product Model and dummy API
    adummy api to check it is running or added product schema  as describe in data.js

---
+++ next-ecom2/pages/api/products/index.js
@@ -0,0 +1,12 @@
+import nc from "next-connect"
+import Product from "../../../model/Product";
+import db from "../../../utilis/db/db"
+const handler = nc()
+
+handler.get( async (req,res)=>{
+    await db.connect();
+    const products = await Product.find({});
+    await db.disconnect()
+    res.send(products)
+})
+export default handler;
